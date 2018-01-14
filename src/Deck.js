e532d4b30e93ae1d56694c70a3425c0153d2643a
mekyleh <kyleholt@Kyles-MacBook-Pro.local>
2018-01-13 18:31:29 -0800

    adding react-native-elements for style

--- src/Deck.js
+++ src/Deck.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import { Animated, Text, View } from 'react-native';
 
 class Deck extends Component {
+  renderCards() {
+    return this.props.data.map(item => {
+      return this.props.renderCard(item);
+    });
+  }
+
   render() {
-    return(
-      <View />
+    return (
+      <View>
+        {this.renderCards()}
+      </View>
     );
   }
 }
